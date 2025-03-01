a022c740a966421d25d3bb050d8083c088384808
DannyCroDev <danielrajic26@outlook.com>
2025-03-01 18:11:07 +0100

    logo on the browser bar

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -9,6 +9,10 @@
     <meta name="description" content="@yield('metaDescription', 'WN8')">
     <meta name="keywords" content="@yield('metaKeywords', 'WN8')">
 
+    <link rel="icon" href="{{ asset('/images/logo-white.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('/images/logo-white.png') }}" type="image/x-icon">
+
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
